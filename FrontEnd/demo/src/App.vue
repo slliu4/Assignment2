cd430f02196e50462682237b8e3fde6d17c9c1b3
Emok1Ng <www4@student.unimelb.edu.au>
2022-04-20 18:13:18 +1000

    add FrontEnd/nav

--- FrontEnd/demo/src/App.vue
+++ FrontEnd/demo/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="nav">
+      <b-nav fill card-header tabs>
+        <b-nav-item to="../view1" exact exact-active-class="active">View1</b-nav-item>
+        <b-nav-item to="../view2" exact exact-active-class="active">View2</b-nav-item>
+        <b-nav-item to="../view3" exact exact-active-class="active">View3</b-nav-item>
+      </b-nav>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -19,5 +25,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#nav {
+  background: #b2b1b3;
 }
 </style>
--- FrontEnd/demo/src/router/index.js
+++ FrontEnd/demo/src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import View1 from '@/components/View1'
+import View2 from '@/components/View2'
+import View3 from '@/components/View3'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/view1',
+      name: 'View1',
+      component: View1
+    },
+    {
+      path: '/view2',
+      name: 'View2',
+      component: View2
+    },
+    {
+      path: '/view3',
+      name: 'View3',
+      component: View3
     }
   ]
 })
